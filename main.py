0118f187080605b0a4d368ed8684d7b06cf7387e
enhouse <elizabeth.house88@gmail.com>
2023-09-21 16:33:33 -0400

    UI design in progress. Using constants to make reuseuable

---
+++ main.py
@@ -0,0 +1,58 @@
+from tkinter import *
+import math
+
+# ----------------------------      CONSTANTS      ------------------------------- #
+PROJECT_NAME = "Password Manager"
+WINDOW_WIDTH = 200
+WINDOW_HEIGHT = 200
+PADDING_WINDOW = 20
+COLOR_PRIMARY = "white"
+COLOR_SECOND = "yellow"
+COLOR_THIRD = "#e2979c"
+COLOR_ERRORS = "#e7305b"
+
+LABEL_FONT = ("Arial", 10)
+LABEL_TEXT_COLOR = "black"
+LABEL_BG_COLOR = "light blue"
+LABEL_COLUMN_START = 0
+LABEL_ROW_START = 1
+
+FONT_NAME = "Courier"
+BOLDED_FONT = ("Arial", 26, "bold")
+LOGO_FILE = "logo.png"
+
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+# creating tk window
+window = Tk()
+window.config(bg=COLOR_PRIMARY, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, pady=PADDING_WINDOW,
+              padx=PADDING_WINDOW)
+window.title(PROJECT_NAME)
+
+# Creating canvas
+canvas = Canvas(width=WINDOW_WIDTH, height=WINDOW_HEIGHT, background=COLOR_PRIMARY, 
+                highlightthickness=0)
+# adding IMAGE logo to middle of screen using a grid system
+logo_img = PhotoImage(file=LOGO_FILE)
+canvas.create_image(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, image=logo_img)
+canvas.grid(column=1, row=0)
+
+# form Label's
+website_label = Label(text="Website:", background=LABEL_BG_COLOR, fg=LABEL_TEXT_COLOR, font=LABEL_FONT)
+website_label.grid(column=LABEL_COLUMN_START, row=LABEL_ROW_START)
+
+email_user_label = Label(text="Email/Username:", background=LABEL_BG_COLOR, fg=LABEL_TEXT_COLOR, font=LABEL_FONT)
+email_user_label.grid(column=LABEL_COLUMN_START, row=LABEL_ROW_START+1)
+
+password_label = Label(text="Password:", background=LABEL_BG_COLOR, fg=LABEL_TEXT_COLOR, font=LABEL_FONT)
+password_label.grid(column=LABEL_COLUMN_START, row=LABEL_ROW_START+2)
+
+
+
+window.mainloop()
